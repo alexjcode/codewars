ba8309a4153051349e39d064243b79765806a296
alexjcode <alexjgraphics@gmail.com>
2019-10-15 16:41:54 -0400

    update find odd int

--- 19-10-07-find-odd-int.js
+++ 19-10-07-find-odd-int.js
@@ -1,11 +1,8 @@
 // Given an array, find the int that appears an odd number of times.
 // There will always be only one integer that appears an odd number of times.
 
-let curr
-const obj = {}
-
-const checker = arr => {
-  if arr.length === 0 {
+const checker = (arr, obj, curr) => {
+  if (arr.length === 0) {
     const oddVal = Object.values(obj).filter( x => { return (parseInt(x, 10) % 2 !== 0) })[0]
     return Object.keys(object).find(key => object[key] === oddVal)
   }
@@ -15,11 +12,12 @@ const checker = arr => {
   } else {
     obj.curr = 1
   }
-  return checker(arr)
+  return checker(arr, obj, curr)
 }
 
 const findOdd = A => {
-  curr = undefined
-  obj = {}
-  return checker(A)
+  const abc = A
+  // curr = undefined
+  // obj = {}
+  return checker(abc, {}, null)
 }
