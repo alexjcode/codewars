77776ca2d5ecf3c8225f8a0c90f607211b7403e9
alexjcode <alexjgraphics@gmail.com>
2020-01-29 15:37:02 -0500

    Add medi-zaku.js

---
+++ medi-zaku.js
@@ -0,0 +1,7 @@
+Every day a new frog is born weight 0g and grows at a rate of 10g/day, maxing out at 100g in 10 days
+
+	On day n, what is the total weight of the frog community?
+
+	On day n, what is the average weight of the average frog?
+
+How might we redesign the model to allow for new frogs to be born on a manually set schedule?
